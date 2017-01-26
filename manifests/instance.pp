812aa5c193e737b3e42cca12e3bfaf752238b093
RamblingCookieMonster <ramblingcookiemonster@users.noreply.github.com>
2017-01-26 14:16:55 -0500

    Add parameters where Windows values may differ

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -3,10 +3,14 @@
 define facts::instance (
   $ensure     = present,
   $command    = 'mkdir -p',
+  $path       = '/bin',
   $facterpath = '/etc/facter/facts.d',
   $factname   = $name,
   $value      = undef,
   $format     = 'txt',
+  $group      = 'root',
+  $owner      = 'root',
+  $mode       = '0664'
 ) {
 
   if versioncmp($::facterversion, '1.7') == -1 {
@@ -16,34 +20,34 @@ define facts::instance (
   exec { "${name} ${command} ${facterpath}":
     command => "${command} ${facterpath}",
     creates => $facterpath,
-    path    => '/bin',
+    path    => $path,
   }
   case $format {
     default: {
       file { "${facterpath}/${factname}.${format}":
         ensure  => $ensure,
         content => "${factname}=${value}",
-        group   => 'root',
-        mode    => '0664',
-        owner   => 'root',
+        group   => $group,
+        mode    => $mode,
+        owner   => $owner,
       }
     }
     'yaml': {
       file { "${facterpath}/${factname}.${format}":
         ensure  => $ensure,
         content => inline_template('<%= { @factname => @value}.to_yaml %>'),
-        group   => 'root',
-        mode    => '0664',
-        owner   => 'root',
+        group   => $group,
+        mode    => $mode,
+        owner   => $owner,
       }
     }
     'json': {
       file { "${facterpath}/${factname}.${format}":
         ensure  => $ensure,
         content => inline_template('<%= { @factname => @value}.to_json %>'),
-        group   => 'root',
-        mode    => '0664',
-        owner   => 'root',
+        group   => $group,
+        mode    => $mode,
+        owner   => $owner,
       }
     }
   }
